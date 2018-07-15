de698c4c3efadcf4924543f066ea7148cf957ec7
cschweigert <cschweigert8@gmail.com>
2018-07-14 17:04:16 -0700

    Adding list of places and map markers

--- js/app.js
+++ js/app.js
@@ -1,9 +1,58 @@
 var map;
+var markers = [];
+
 function initMap() {
-  // TODO: use a constructor to create a new map JS object. You can use the coordinates
-  // we used, 40.7413549, -73.99802439999996 or your own!
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7413549, lng: -73.99802439999996},
-      zoom: 13
-  });
+    // ** Create the map **
+    map = new google.maps.Map(document.getElementById('map'), {
+        //center on Phoenix, AZ to start
+        center: {lat: 33.6050991, lng: -112.4052386},
+        zoom: 13
+    });
+
+    // ** Create the markers **
+    // Create an array of markers from the list of places on init
+    for (var i = 0; i < viewModel.places.length; i++) {
+        // Get the lat and lng from the viewModel and create a new latlng object
+        var position = new google.maps.LatLng(viewModel.places[i].lat(),viewModel.places[i].lng());
+        // Get the name of the location from the viewModel
+        var title = viewModel.places[i].name();
+
+        console.log(position + ' ' + title);
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            //icon: defaultIcon,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+/*         // Create an onclick event to open the large infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        }); */
+    }
+
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+      bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+
+   /*  //create search box
+    var searchBox = new google.maps.places.SearchBox(
+    document.getElementById('places-search'));
+    // Bias the searchbox to within the bounds of the map.
+    searchBox.setBounds(map.getBounds()); */
 }
--- js/models/map-model.js
+++ js/models/map-model.js
@@ -1,5 +1,17 @@
-var ViewModel = function(){
-    this.name = ko.observable('Test Place');
-}
+var places = [];
 
-ko.applyBindings(new ViewModel());
+var Place = function(name, lat, lng) {
+    this.name = ko.observable(name);
+    this.lat = ko.observable(lat);
+    this.lng = ko.observable(lng);
+}
+ 
+// The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
+var viewModel = {
+    places: [
+        new Place("Grand Canyon National Park", 36.1069652, -112.1129972),
+        new Place("Lost Dutchman State Park", 33.4565, -111.4782),
+        ]
+};
+ 
+ko.applyBindings(viewModel);
