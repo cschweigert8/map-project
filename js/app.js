e9c329d9045cd8458947116de00b287ea9198004
cschweigert <cschweigert8@gmail.com>
2018-07-14 22:23:39 -0700

    a lot of stuff

--- js/app.js
+++ js/app.js
@@ -26,79 +26,98 @@ function initMap() {
             animation: google.maps.Animation.DROP,
             id: i
         });
-        // Push the marker to our array of markers.
+        // Add the created marker to the markers array
         markers.push(marker);
-        // Create an onclick event to open the large infowindow at each marker.
+        // Add listener to open the infoWindow for the clicked marker
         marker.addListener('click', function() {
             populateInfoWindow(this, infowindow);
         });
-/*         // Two event listeners - one for mouseover, one for mouseout,
-        // to change the colors back and forth.
-        marker.addListener('mouseover', function() {
-            this.setIcon(highlightedIcon);
-        });
-        marker.addListener('mouseout', function() {
-            this.setIcon(defaultIcon);
-        }); */
     }
 
+    // Extends the bounds of the map for the initial markers
     var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < markers.length; i++) {
       markers[i].setMap(map);
       bounds.extend(markers[i].position);
     }
     map.fitBounds(bounds);
 
-   /*  //create search box
-    var searchBox = new google.maps.places.SearchBox(
-    document.getElementById('places-search'));
-    // Bias the searchbox to within the bounds of the map.
-    searchBox.setBounds(map.getBounds()); */
 }
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
+// The populateInfoWindow function creates an info window when the
+// marker is click as well as when the place is clicked from the
+// list on the sidebar.
 function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
+    var address = "";
+    var geocoder = new google.maps.Geocoder();
+    // Checks to see if the infoWindow is already open for that marker
+    // If it is, the infoWindow isn't populated
     if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
+        // Reset infoWindow for marker
         infowindow.setContent('');
         infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
+        // Clear marker object when window is closed
         infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
         });
-        /* var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-        // In case the status is OK, which means the pano was found, compute the
-        // position of the streetview image, then calculate the heading, then get a
-        // panorama from that and set the options
-        function getStreetView(data, status) {
-        if (status == google.maps.StreetViewStatus.OK) {
-            var nearStreetViewLocation = data.location.latLng;
-            var heading = google.maps.geometry.spherical.computeHeading(
-            nearStreetViewLocation, marker.position);
-            infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-            var panoramaOptions = {
-                position: nearStreetViewLocation,
-                pov: {
-                heading: heading,
-                pitch: 30
+
+        // Get the formatted address for the place
+        geocoder.geocode({ 'latLng': marker.position}, function(results, status) {
+            // Geocoder didn't error out
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    // Geocoder returned successfully with a result
+                    // Add address to the infoWindow
+                    address = results[1].formatted_address;
+                    infowindow.setContent('<div>' + marker.title + '</div>' + '</br>' + '<div>' + address + '</div>');
+                } else {
+                    // Geocoder didn't error out but didn't find any address for the place
+                    infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Results Found</div>');
                 }
-            };
-            var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), panoramaOptions);
-        } else {
-            infowindow.setContent('<div>' + marker.title + '</div>' +
-            '<div>No Street View Found</div>');
-        }
-        }
-        // Use streetview service to get the closest streetview image within
-        // 50 meters of the markers position
-        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView); */
-        // Open the infowindow on the correct marker.
+            } else {
+                // Geocoder encountered an error
+                infowindow.setContent('<div>' + marker.title + '</div>' + '<div>Geocoder failed! Error: ' + status + '</div>');
+            }
+            });
+
+        // open infoWindow
         infowindow.open(map, marker);
+    }
+}
+
+// This function loops through the array of markers to find the marker
+// that has the same title as the clicked list item. When the list item
+// is found, it calls the onClick event for the specified marker, which
+// populates and opens the infoWindow for that marker.
+function openInfoWindowForClickedPlace(name){
+    // loop through markers array
+    for (var i = 0; i < markers.length; i++) {
+        // check if clicked name is the same as marker title
+        // if true, run onclick event for the matching marker
+        if(name === markers[i].title){ google.maps.event.trigger(markers[i], 'click'); };
+    }
+}
+
+function filterPlacesList(activity){
+    for (var i = 0; i < viewModel.places.length; i++){
+        if(!viewModel.places[i].activities().includes(String(activity))){ 
+            viewModel.places[i].showPlace(false);
+            hideMarker(viewModel.places[i].name());
+        }else {
+            viewModel.places[i].showPlace(true);
+            showMarker(viewModel.places[i].name());
+        }
+    }
+}
+
+function hideMarker(title) {
+    for (var i = 0; i < markers.length; i++) {
+      if(title === markers[i].title){markers[i].setMap(null);}
+    }
+}
+
+function showMarker(title) {
+    for (var i = 0; i < markers.length; i++) {
+      if(title === markers[i].title){markers[i].setMap(map);}
     }
 }
--- js/models/map-model.js
+++ js/models/map-model.js
@@ -1,17 +1,27 @@
 var places = [];
 
-var Place = function(name, lat, lng) {
+var Place = function(name, lat, lng, activities) {
     this.name = ko.observable(name);
     this.lat = ko.observable(lat);
     this.lng = ko.observable(lng);
+    this.activities = ko.observableArray(activities);
+    this.showPlace = ko.observable(true);
+
+/*     toggleShowPlace = function(){
+        console.log("toggle show place");
+        showPlace(false);
+    } */
 }
  
 // The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
 var viewModel = {
     places: [
-        new Place("Grand Canyon National Park", 36.1069652, -112.1129972),
-        new Place("Lost Dutchman State Park", 33.4565, -111.4782),
-        ]
-};
+        new Place("Grand Canyon National Park", 36.1069652, -112.1129972, ["Hiking", "Camping", "Swimming", "Biking"]),
+        new Place("Lost Dutchman State Park", 33.4565, -111.4782, ["Hiking", "Biking"]),
+        new Place("White Tank Mountain Regional Park", 33.4709558, -112.2353819, ["Hiking", "Biking"]),
+        new Place("Tonto Natural Bridge State Park", 33.4709558, -114.3518127, ["Hiking", "Biking", "Swimming"]),
+        new Place("Usery Mountain Regional Park", 33.456737, -111.734434, ["Hiking"])
+    ]
+}
  
 ko.applyBindings(viewModel);
